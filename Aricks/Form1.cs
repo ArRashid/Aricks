481c504a0840adad8aca717aa8e76bc3b8ec6c83
ArRashid <ar.rashid.mondal@gmail.com>
2021-08-02 21:58:27 +0530

    Frist commit Aricks Versation 2

---
+++ Aricks/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aricks
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bacupsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            mainpanel.Controls.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+            
+           
+            mainpanel.Controls.Add(UserControl1.Instance);
+           
+            
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+                    }
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+            mainpanel.Controls.Add(UserControl1.Instance);
+        }
+    }
+}
---
+++ Aricks/UserControl1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aricks
+{
+    public partial class UserControl1 : UserControl
+    {
+        private static UserControl1 _instance;
+        public static UserControl1 Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new UserControl1();
+                } return _instance;
+            }
+        }
+
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_addac_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(" helllo ar");
+        }
+    }
+}
